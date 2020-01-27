9b602944355cde3f3d6d8be623d7ec66b780685a
tonn24 <tonn.toomsalu@gmail.com>
2020-01-27 11:00:17 +0200

    Add windows form

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Toomsalu_taringumang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TT_alustaUutManguButton = new System.Windows.Forms.Button();
+            this.TT_mangibJukuButton = new System.Windows.Forms.Button();
+            this.TT_mangibPeeterButton = new System.Windows.Forms.Button();
+            this.TT_jukuTaring1 = new System.Windows.Forms.TextBox();
+            this.TT_jukuTaring2 = new System.Windows.Forms.TextBox();
+            this.TT_peeterTaring2 = new System.Windows.Forms.TextBox();
+            this.TT_peeterTaring1 = new System.Windows.Forms.TextBox();
+            this.TT_jukuPunktidText = new System.Windows.Forms.TextBox();
+            this.TT_peeterPunktidText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.Location = new System.Drawing.Point(264, 338);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // TT_alustaUutManguButton
+            // 
+            this.TT_alustaUutManguButton.BackColor = System.Drawing.Color.Green;
+            this.TT_alustaUutManguButton.Location = new System.Drawing.Point(290, 259);
+            this.TT_alustaUutManguButton.Name = "TT_alustaUutManguButton";
+            this.TT_alustaUutManguButton.Size = new System.Drawing.Size(174, 47);
+            this.TT_alustaUutManguButton.TabIndex = 1;
+            this.TT_alustaUutManguButton.Text = "Alustuut mängu";
+            this.TT_alustaUutManguButton.UseVisualStyleBackColor = false;
+            // 
+            // TT_mangibJukuButton
+            // 
+            this.TT_mangibJukuButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.TT_mangibJukuButton.Location = new System.Drawing.Point(140, 240);
+            this.TT_mangibJukuButton.Name = "TT_mangibJukuButton";
+            this.TT_mangibJukuButton.Size = new System.Drawing.Size(126, 38);
+            this.TT_mangibJukuButton.TabIndex = 2;
+            this.TT_mangibJukuButton.Text = "Mängib Juku";
+            this.TT_mangibJukuButton.UseVisualStyleBackColor = false;
+            // 
+            // TT_mangibPeeterButton
+            // 
+            this.TT_mangibPeeterButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.TT_mangibPeeterButton.Location = new System.Drawing.Point(503, 240);
+            this.TT_mangibPeeterButton.Name = "TT_mangibPeeterButton";
+            this.TT_mangibPeeterButton.Size = new System.Drawing.Size(119, 38);
+            this.TT_mangibPeeterButton.TabIndex = 3;
+            this.TT_mangibPeeterButton.Text = "Mängib Peeter";
+            this.TT_mangibPeeterButton.UseVisualStyleBackColor = false;
+            // 
+            // TT_jukuTaring1
+            // 
+            this.TT_jukuTaring1.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_jukuTaring1.Location = new System.Drawing.Point(140, 162);
+            this.TT_jukuTaring1.Name = "TT_jukuTaring1";
+            this.TT_jukuTaring1.Size = new System.Drawing.Size(37, 26);
+            this.TT_jukuTaring1.TabIndex = 4;
+            // 
+            // TT_jukuTaring2
+            // 
+            this.TT_jukuTaring2.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_jukuTaring2.Location = new System.Drawing.Point(229, 162);
+            this.TT_jukuTaring2.Name = "TT_jukuTaring2";
+            this.TT_jukuTaring2.Size = new System.Drawing.Size(37, 26);
+            this.TT_jukuTaring2.TabIndex = 5;
+            // 
+            // TT_peeterTaring2
+            // 
+            this.TT_peeterTaring2.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_peeterTaring2.Location = new System.Drawing.Point(585, 162);
+            this.TT_peeterTaring2.Name = "TT_peeterTaring2";
+            this.TT_peeterTaring2.Size = new System.Drawing.Size(37, 26);
+            this.TT_peeterTaring2.TabIndex = 6;
+            // 
+            // TT_peeterTaring1
+            // 
+            this.TT_peeterTaring1.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_peeterTaring1.Location = new System.Drawing.Point(503, 162);
+            this.TT_peeterTaring1.Name = "TT_peeterTaring1";
+            this.TT_peeterTaring1.Size = new System.Drawing.Size(37, 26);
+            this.TT_peeterTaring1.TabIndex = 7;
+            // 
+            // TT_jukuPunktidText
+            // 
+            this.TT_jukuPunktidText.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_jukuPunktidText.Location = new System.Drawing.Point(140, 100);
+            this.TT_jukuPunktidText.Name = "TT_jukuPunktidText";
+            this.TT_jukuPunktidText.Size = new System.Drawing.Size(126, 26);
+            this.TT_jukuPunktidText.TabIndex = 8;
+            // 
+            // TT_peeterPunktidText
+            // 
+            this.TT_peeterPunktidText.BackColor = System.Drawing.SystemColors.Menu;
+            this.TT_peeterPunktidText.Location = new System.Drawing.Point(503, 100);
+            this.TT_peeterPunktidText.Name = "TT_peeterPunktidText";
+            this.TT_peeterPunktidText.Size = new System.Drawing.Size(126, 26);
+            this.TT_peeterPunktidText.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(136, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Esimene mängija Juku";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(499, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Teine mängija Peeter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(169, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Punktid";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(531, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Punktid";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(73, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Tulemus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TT_peeterPunktidText);
+            this.Controls.Add(this.TT_jukuPunktidText);
+            this.Controls.Add(this.TT_peeterTaring1);
+            this.Controls.Add(this.TT_peeterTaring2);
+            this.Controls.Add(this.TT_jukuTaring2);
+            this.Controls.Add(this.TT_jukuTaring1);
+            this.Controls.Add(this.TT_mangibPeeterButton);
+            this.Controls.Add(this.TT_mangibJukuButton);
+            this.Controls.Add(this.TT_alustaUutManguButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button TT_alustaUutManguButton;
+        private System.Windows.Forms.Button TT_mangibJukuButton;
+        private System.Windows.Forms.Button TT_mangibPeeterButton;
+        private System.Windows.Forms.TextBox TT_jukuTaring1;
+        private System.Windows.Forms.TextBox TT_jukuTaring2;
+        private System.Windows.Forms.TextBox TT_peeterTaring2;
+        private System.Windows.Forms.TextBox TT_peeterTaring1;
+        private System.Windows.Forms.TextBox TT_jukuPunktidText;
+        private System.Windows.Forms.TextBox TT_peeterPunktidText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
